1329c084895f9f8b9894b6508094539b7ea2f50b
Markus Meier <markus.d.meier@gmail.com>
2019-06-29 12:50:38 +0200

    JavaScript

--- JavaScript/README.md
+++ JavaScript/README.md
@@ -6,3 +6,8 @@
 | gulp       | JS task runner; newer & faster; tasks defined by code
 | npm        | maven for JS; part of nodejs
 | webpack    | module bundler for JS, HTML, CSS
+
+## Good to know
+### Duck typing
+> When I see a bird that walks like a duck and swims like a duck and quacks like a duck, I call that bird a duck.
+* Specific to **JS**, Python, PHP (amongst others).
---
+++ JavaScript/typescript/demo.ts
@@ -0,0 +1,42 @@
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+class MyStudent implements Person {
+    firstName: string;
+    lastName: string;
+    middleName: string;
+}
+
+class YoungStudent extends Student {
+    age: number;
+}
+
+function greetPerson(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+function printString(person: string) {
+    return "Hello, " + person;
+}
+
+let userString = "Jane User";
+let userPerson = {"firstName": "Markus", lastName: "Meier"};
+let userStudent = new Student("This", "I.", "Sparta");
+
+let myStudent = new MyStudent();
+let youngStudent = new YoungStudent("a", "b", "c");
+
+document.body.textContent = printString(userString);
+document.body.textContent += greetPerson(userPerson);
+document.body.textContent += greetPerson(userStudent);
+document.body.textContent += greetPerson(myStudent);
+document.body.textContent += greetPerson(youngStudent);
